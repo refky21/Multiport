f64aed69bb705b6735d71b154a44bfa6ff6c50a0
Tumbas Digital <refkysatria21@gmail.com>
2024-08-03 09:15:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -61,14 +61,14 @@ VALIDITY() {
 }
 IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 
-echo $IZIN
-exit 0
+
 if [ $MYIP = $IZIN ]; then
     echo -e "\e[32mPermission Accepted...\e[0m"
     VALIDITY
 else
     echo -e "\e[31mPermission Denied!\e[0m"
     echo -e "\e[31mPlease buy script first\e[0m"
+    echo $IZIN
     rm -f setup.sh
     exit 0
 fi
